eecd890e3c40ec163994de3e0b3e727a33e0ed5f
Trenton Scott <InsaneGriot@Outlook.com>
2025-05-13 02:11:23 -0400

    Added functionality

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,23 +1,45 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace DiceRoller;
 
 /// <summary>
 /// Interaction logic for MainWindow.xaml
 /// </summary>
+
+
+public class DiceRoll
+{
+    public static Random random = new Random();
+    public static int D20() => random.Next(1, 21);
+    public static int D12() => random.Next(1, 13);
+    public static int D8() => random.Next(1, 9);
+    public static int D6() => random.Next(1, 7);
+    public static int D4() => random.Next(1, 5);
+    public static int D100() => random.Next(1, 101);
+
+
+}
+
 public partial class MainWindow : Window
 {
     public MainWindow()
     {
         InitializeComponent();
+    }
+    private void OperationButton_Click(object sender, RoutedEventArgs e)
+    {
+
+        if (sender == d20Button)
+            resultLabel.Content = DiceRoll.D20().ToString();
+        if (sender == d12Button)
+            resultLabel.Content = DiceRoll.D12().ToString();
+        if (sender == d8Button)
+            resultLabel.Content = DiceRoll.D8().ToString();
+        if (sender == d6Button)
+            resultLabel.Content = DiceRoll.D6().ToString();
+        if (sender == d4Button)
+            resultLabel.Content = DiceRoll.D4().ToString();
+        if (sender == d100Button)
+            resultLabel.Content = DiceRoll.D100().ToString();
     }
 }
